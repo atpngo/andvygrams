a98d44fbe7b11c670226a0eecf9fd45e31ec8b35
Andy Ngo <andytpngo@g.ucla.edu>
2024-08-24 01:59:11 -0400

    update connection

--- app/components/Socket.js
+++ app/components/Socket.js
@@ -2,8 +2,13 @@ import { io } from "socket.io-client";
 
 const { SOCKET_SERVER_URL } = process.env;
 // Local dev lol
-let ADDRESS = "https://socket-server-9qm5.onrender.com";
+// let ADDRESS = "https://socket-server-9qm5.onrender.com";
+// let ADDRESS = "https://socket-server-e8c7ac523508.herokuapp.com";
+// let ADDRESS = "http://localhost:4000"
 let PORT = 80
-export const socket = io(ADDRESS+":"+PORT, {autoConnect: false})
+// export const socket = io(ADDRESS+":"+PORT, {autoConnect: false})
+// export const socket = io("http://localhost:4000", {autoConnect: false})
+// export const socket = io("https://socket-server-e8c7ac523508.herokuapp.com", {autoConnect: false})
+export const socket = io("https://socket-server-9qm5.onrender.com", {autoConnect: false})
 // "prod"
 // export const socket = io("http://localhost:4000", {autoConnect: false})
