3a2ba7b5bc03744c273e6936a578ff730964f19a
Andy Ngo <andytpngo@g.ucla.edu>
2024-08-24 00:35:10 -0400

    update server

--- app/components/Socket.js
+++ app/components/Socket.js
@@ -2,6 +2,8 @@ import { io } from "socket.io-client";
 
 const { SOCKET_SERVER_URL } = process.env;
 // Local dev lol
-export const socket = io("https://socket-server-9qm5.onrender.com", {autoConnect: false})
+let ADDRESS = "https://socket-server-9qm5.onrender.com";
+let PORT = 4000
+export const socket = io(ADDRESS+":"+PORT, {autoConnect: false})
 // "prod"
 // export const socket = io("http://localhost:4000", {autoConnect: false})
